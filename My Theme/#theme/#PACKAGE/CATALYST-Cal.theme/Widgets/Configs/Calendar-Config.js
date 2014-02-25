8526f15e9866605a4dbe17cd9fad7ed72a68b34e
Manik Rathee <manikrathee@gmail.com>
2014-02-25 14:15:34 -0800

    updated file structure

--- My Theme/#theme/#PACKAGE/CATALYST-Cal.theme/Widgets/Configs/Calendar-Config.js
+++
@@ -1,8 +0,0 @@
-// Enable Lockscreen Calendar
-var CalendarEnabled = true;
-
-// Enable Springboard Calendar
-var EnableSpringBoardCalendar = true;
-
-// Enable Calendar Grid
-var EnabledGrid = false;
